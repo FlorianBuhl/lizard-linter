bdb626ac79c24a3cb1ecc597a501dd48e07ab7c5
FlorianBuhl <florian.buhl@freenet.de>
2020-10-16 20:08:41 +0200

    Ignoring Javascript test files from es lint.
    Ignoring of tes files and correcting of test files to eslint. just for 
    safety.

--- spec/files/JavaScript/bad.js
+++ spec/files/JavaScript/bad.js
@@ -1,10 +1,10 @@
 function bad_function() {
-  var i = 0
-  var toReturn
-  for (i = 0; i < 10; i++) {
+  let i = 0;
+  let toReturn;
+  for (i = 0; i < 10; i += 1) {
     if (i === 3) {
-      toReturn = 10
+      toReturn = 10;
     }
   }
-  return toReturn
+  return toReturn;
 }
