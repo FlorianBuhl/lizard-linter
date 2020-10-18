a003d7fd789da8cba8e17ccfd183e5b1edb2990d
FlorianBuhl <florian.buhl@freenet.de>
2020-10-18 12:13:12 +0200

    Adapted python file to spec.

--- spec/files/very_bad.py
+++ spec/files/very_bad.py
@@ -63,24 +63,3 @@ class BadTestClass:
             if entry["date"] == date:
                 return entry["value"]
         return 0
-
-    def switch_case_function(self, date):
-        """ a function with switch case"""
-
-        if(self.entries[0]["date"] == date):
-
-            switcher = {
-                1: "January",
-                2: "February",
-                3: "March",
-                4: "April",
-                5: "May",
-                6: "June",
-                7: "July",
-                8: "August",
-                9: "September",
-                10: "October",
-                11: "November",
-                12: "December"
-            }
-        return switcher.get(self.test, "Invalid month")
